e348b8ec61527d9efa9bd8b744bcfb6766d840eb
Ted <tedlpascua@gmail.com>
2017-04-12 00:43:14 -0700

    made index.html outside of the game access folder

--- game/main_core/game.js
+++ game/main_core/game.js
@@ -1,10 +1,10 @@
 var GameIntroState = {
   preload: function(){
-      this.load.image('logo', '../assets/menu/Book_1.png');
-      this.load.image('preLoadBar', '../assets/menu/Stupid_Bar.png');
+      this.load.image('logo', 'game/assets/menu/Book_1.png');
+      this.load.image('preLoadBar', 'game/assets/menu/Stupid_Bar.png');
 
-      this.load.image('load_ball', '../assets/menu/Load_Ball.png');
-      this.load.image('wordLoading', '../assets/menu/loading.png');
+      this.load.image('load_ball', 'game/assets/menu/Load_Ball.png');
+      this.load.image('wordLoading', 'game/assets/menu/loading.png');
   },
 
   create: function() {
