0562d5696ded4c11a0d2f344dfaa90a90c2ba0e5
Ted <tedlpascua@gmail.com>
2017-06-17 19:25:22 -0700

    optimize and simplifed pickup, grab, release mouse functionality

--- game/main_core/tools.js
+++ game/main_core/tools.js
@@ -1,57 +1,28 @@
 //---------------------------------------------------------------------
 //  1_Tools_Control
 //  2_Camera_Control
-//  3_Pinpoint_Rectangle
-//  4_Object_Creation
 //  5_Mouse_Control
-//  6_Adding_Collisions
+//  4_Object_Creation
+//  3_Pinpoint_Rectangle
 //  7_Creation
 //---------------------------------------------------------------------
-function editorCamera(){
-}
-
-function gameModeSetup(){
-  var sameOld = JSON.parse(localStorage.getItem("sameOld"));
-  //sameOld = "none";
-  if(sameOld == version){
-    console.log("I MADE IT THROUGH");
-    var levelAiPlacement = JSON.parse(localStorage.getItem("levelAiPlacement"));
-    var levelBlockPlacement = JSON.parse(localStorage.getItem("levelBlockPlacement"));
-    var levelStmp = JSON.parse(localStorage.getItem("levelStmp"));
-    var levelSflo = JSON.parse(localStorage.getItem("levelSflo"));
-  }
-
-  if(levelAiPlacement == null) return;
-  if(levelBlockPlacement == null) return;
-  console.log("AI LOADED", levelAiPlacement.length);
-  console.log("Block Loaded", levelBlockPlacement);
-  for(var i = 0; i < levelBlockPlacement.length; i++){
-    console.log("block", i);
-    creation = "existance";
-    createHere2(levelBlockPlacement[i].x, levelBlockPlacement[i].y, 
-        levelBlockPlacement[i].width, levelBlockPlacement[i].height);
-  }
-
-  for(var i = 0; i < levelAiPlacement.length; i++){
-    console.log("ai", i);
-    creation = levelAiPlacement[i].name;
-    createHere2(levelAiPlacement[i].x, levelAiPlacement[i].y, 0, 0);
-  }
-
-  for(var i = 0; i < levelStmp.length; i++){
-    creation = levelStmp[i].name;
-    createHere2(levelStmp[i].x, levelStmp[i].y, 0, 0);
-  }
-
-  for(var i = 0; i < levelSflo.length; i++){
-    creation = levelSflo[i].name;
-    createHere2(levelSflo[i].x, levelSflo[i].y, 0, 0);
-  }
-  console.log(levelSflo, "FLO");
-}
 //--------------------------------------
 //  1_Tools_Control
 //--------------------------------------
+function toolsSetup(){
+  setupMouse();
+  toolControls();
+  create6pinPoints();
+}
+
+function setupMouse(){
+  makeMouseBody();
+  game.input.onDown.add(click, this);
+  game.input.onUp.add(release, this);
+  game.input.addMoveCallback(moveWith, this);
+  mouse.body.onBeginContact.add(grabBody, mouse);
+}
+
 function toolControls(){
   buttonC = game.input.keyboard.addKey(Phaser.Keyboard.C);
   buttonC.onDown.add(createHere, this);
@@ -75,23 +46,12 @@ function toolControls(){
   moveDown = game.input.keyboard.addKey(Phaser.Keyboard.S);
   moveDown.onDown.add(cameraMoveDown, this);
 }
-
-function toolsSetup(){
-  setupMouse();
-  toolControls();
-  create6pinPoints();
-  //var levelAiPlacement = JSON.parse(localStorage.getItem("levelAiPlacement"));
-  //var levelBlockPlacement = JSON.parse(localStorage.getItem("levelBlockPlacement"));
-  //var levelStmp = JSON.parse(localStorage.getItem("levelStmp"));
-  //var levelSflo = JSON.parse(localStorage.getItem("levelSflo"));
-}
 //---------------------------------------
 // 2_Camera_Control
 //---------------------------------------
 function cameraMoveRight(){
   game.camera.x += 300;
 }
-
 function cameraMoveLeft(){
   game.camera.x -= 300;
 }
@@ -99,20 +59,12 @@ function cameraMoveLeft(){
 function cameraMoveDown(){
   game.camera.y += 300;
 }
-
 function cameraMoveUp(){
   game.camera.y -= 300;
 }
 //--------------------------------------
 //  3_Pinpoint_Rectangle
 //--------------------------------------
-var pinTargetID = 0;
-var pinPointOn = false;
-var pinHeight = 0;
-var pinWidth = 0;
-var pinAtX = 0;
-var pinAtY = 0;
-
 function create6pinPoints(){
   pinTR = game.add.sprite(-1000, -500, 'click');//Top Left
   pinTM = game.add.sprite(-1000, -500, 'click');//Top Middle
@@ -180,9 +132,9 @@ function resizeOn(body){
   pinHeight = pinBR.body.y - pinTR.body.y;
   pinAtY = pinTL.body.y + pinHeight/2;
 
-
   pinWidth = pinTR.body.x - pinTL.body.x;
   pinAtX = pinTL.body.x + pinWidth/2;
+
 }
 
 function moveWithTR(){
@@ -318,32 +270,14 @@ function reviveExist(width, height, xoo, yoo){
 }
 
 
-function existance(width, height, xoo, yoo){
-  existingBlocks[pinTargetID-1].destroy();
-  var nid = pinTargetID;
-  var box = game.add.sprite(0,0, 'exist');
-  box.width = width;
-  box.height= height;
-  box.x = xoo;
-  box.y = yoo;
-  game.physics.p2.enable(box);
-  box.frame = 0;
-  box.body.fixedRotation = true;
-
-  box.body.setMaterial(boxMaterial);
-  box.body.static = true;
-  box.hooks = true;
-
-  console.log("Existing Blocks", existingBlocks.length);
-  box.eid = nid;
-  box.body.eid = nid;
-
-  box.body.indestructible = true;
-  console.log(box.eid);
-  console.log(box.body.eid);
+function pinEdit(width, height, xoo, yoo){
+  var box = existingBlocks[pinTargetID-1];
+  box.width  = width;
+  box.height = height;
+  box.body.setRectangle(box.width, box.height);
+  box.reset(xoo, yoo);
   game.world.bringToTop(foreground);
-
-  existingBlocks[pinTargetID-1] = box;
+  console.log("Existing Blocks", box.length);
 }
 
 
@@ -365,51 +299,26 @@ var mouseHolding = false;
 var targetBody;
 var bodyCheck = false;
 var bodyPast = false;
+var mouseBody;
+var pinPointOn = false;
 
-function setupMouse(){
+function makeMouseBody(){
   mouseBody = new p2.Body();
   game.physics.p2.world.addBody(mouseBody);
-
   mouse = game.add.sprite(300, 100, 'click');
   mouse.scale.setTo(0.5, 0.5);
   game.physics.p2.enable(mouse);
   mouse.body.fixedRotation = true; 
   mouseMaterial = game.physics.p2.createMaterial('mouseMaterial', mouse.body);
   mouse.body.static = true;
-
-  // attach pointer events
-  game.input.onDown.add(click, this);
-  game.input.onUp.add(release, this);
-  game.input.addMoveCallback(moveWith, this);
-
   mouse.body.onBeginContact.add(grabBody, mouse);
-}
-
-function setupMouseAlt(){
-  mouseAlt = game.add.sprite(-1000, 100, 'click');
-  mouseAlt.scale.setTo(0.5, 0.5);
-  game.physics.p2.enable(mouseAlt);
-  mouseAlt.body.fixedRotation = true; 
-  mouseMaterial = game.physics.p2.createMaterial('mouseMaterial', mouseAlt.body);
-  mouseAlt.body.static = false;
-  // attach pointer events
-  //mouseAlt.body.onBeginContact.add(grabBody, mouseAlt);
+  mouse.body.data.gravityScale = 0;
 }
 
 function click(pointer) {
-  //addPoint(pointer);
-  mouse.body.onBeginContact.add(grabBody, mouse);
-  console.log("before XY", pointer.x, pointer.y);
-  console.log("after XY", pointer.x, pointer.y);
   mouse.body.x = pointer.x + game.camera.x;
   mouse.body.y = pointer.y + game.camera.y;
-
   mouse.body.static = false;
-  mouse.body.data.gravityScale = 0;
-
-  mouse.body.velocity.x = 0;
-  mouse.body.velocity.y = 0;
-
   mouseHolding = true;
 }
 
@@ -417,33 +326,26 @@ function release() {
   mouse.body.static = true;
   mouse.body.velocity.x = 0;
   mouse.body.velocity.y = 0;
+  mouseHolding = false;
 
   if(targetBody == null) return;
-  mouseHolding = false;
   bodyCheck = false;
   targetBody.static = bodyPast;
   targetBody.velocity.x = 0;
   targetBody.velocity.y = 0;
-  mouse.body.x = -50;
-  mouse.body.y = -50;
-
   targetBody = null;
-  console.log("mouse hold off");
+
   if(pinPointOn){
-    console.log(" did it work");
-    console.log("PIN AXIS: ", pinAtX, pinAtY);
-    existance(pinWidth, pinHeight, pinAtX, pinAtY);
+    pinEdit(pinWidth, pinHeight, pinAtX, pinAtY);
   }
   pinPointOn = false;
 }
 
 function grabBody(body1, body2){
-  console.log(body1);
   if(bodyCheck) return;
   if(body1 == null) return;
   mouse.body.static = true;
   bodyCheck = true;
-  console.log("Am I touching something");
   bodyPast = body1.static;
   body1.static = true;
   lastTargetBody = body1;
@@ -453,7 +355,6 @@ function grabBody(body1, body2){
   pinTactY = mouse.body.y - body1.y;
 
   if(body1.sprite.hname != null) pinPointOn = true;
-  //surrondPinpoint(body1);
 }
 
 function releaseBody(body1, body2){
@@ -472,50 +373,14 @@ function moveWith(pointer) {
     targetBody.y = mouse.body.y;
 
     targetBody.x = mouse.body.x - pinTactX
-      targetBody.y = mouse.body.y - pinTactY;
+    targetBody.y = mouse.body.y - pinTactY;
 
     targetBody.velocity.x = 0;
     targetBody.velocity.y = 0;
-    //surrondPinpoint(targetBody);
     hooksBaseOn(targetBody);
   }
 }
 
-//---------------------------
-// 6_Adding_Collisions
-//--------------------------
-var squarePoint = [];
-var topLeft;
-var topRight;
-var bottomLeft;
-var bottomRight;
-
-function addPoint(pointer){
-  var pointway = [];
-  pointway.x = pointer.x + game.camera.x;
-  pointway.y = pointer.y + game.camera.y;
-  squarePoint.push(pointway);
-  console.log("ADD IN", squarePoint);
-}
-
-function squareTruth(point1, point2){
-  if(point1.x >= point2.x){
-    if(point1.y >= point2.y){
-      bottomRight = point1;
-    }
-    else{
-      topRight= point1;
-    }
-  }
-  else{
-    if(point1.y >= point2.y){
-      bottomLeft = point1;
-    }
-    else{
-      topLeft= point1;
-    }
-  }
-}
 //--------------------------
 //  7_Creation
 //--------------------------
@@ -552,9 +417,6 @@ var creationList = [
 }
 ];
 
-function lookAtInfo(object){
-
-}
 var chx = 0;
 var chy = 0;
 function createHere(){
@@ -591,3 +453,58 @@ function createHere2(x,y,width,height){
   }
   console.log("creation", creation);
 }
+
+//-------------------------
+//  Load_And_Save_Map
+//--------------------------
+function loadStorage(){
+  //var levelAiPlacement = JSON.parse(localStorage.getItem("levelAiPlacement"));
+  //var levelBlockPlacement = JSON.parse(localStorage.getItem("levelBlockPlacement"));
+  //var levelStmp = JSON.parse(localStorage.getItem("levelStmp"));
+  //var levelSflo = JSON.parse(localStorage.getItem("levelSflo"));
+}
+
+function gameModeSetup(){
+  var sameOld = JSON.parse(localStorage.getItem("sameOld"));
+  //sameOld = "none";
+  if(sameOld == version){
+    console.log("I MADE IT THROUGH");
+    var levelAiPlacement = JSON.parse(localStorage.getItem("levelAiPlacement"));
+    var levelBlockPlacement = JSON.parse(localStorage.getItem("levelBlockPlacement"));
+    var levelStmp = JSON.parse(localStorage.getItem("levelStmp"));
+    var levelSflo = JSON.parse(localStorage.getItem("levelSflo"));
+  }
+
+  if(levelAiPlacement == null) return;
+  if(levelBlockPlacement == null) return;
+  console.log("AI LOADED", levelAiPlacement.length);
+  console.log("Block Loaded", levelBlockPlacement);
+  for(var i = 0; i < levelBlockPlacement.length; i++){
+    console.log("block", i);
+    creation = "existance";
+    createHere2(levelBlockPlacement[i].x, levelBlockPlacement[i].y, 
+        levelBlockPlacement[i].width, levelBlockPlacement[i].height);
+  }
+
+  for(var i = 0; i < levelAiPlacement.length; i++){
+    console.log("ai", i);
+    creation = levelAiPlacement[i].name;
+    createHere2(levelAiPlacement[i].x, levelAiPlacement[i].y, 0, 0);
+  }
+
+  for(var i = 0; i < levelStmp.length; i++){
+    creation = levelStmp[i].name;
+    createHere2(levelStmp[i].x, levelStmp[i].y, 0, 0);
+  }
+
+  for(var i = 0; i < levelSflo.length; i++){
+    creation = levelSflo[i].name;
+    createHere2(levelSflo[i].x, levelSflo[i].y, 0, 0);
+  }
+  console.log(levelSflo, "FLO");
+}
+
+function editorCamera(){
+}
+
+
