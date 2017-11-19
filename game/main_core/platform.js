391d8875f6ab22a25485721518488b71c6cb1d05
Ted Pascua <tedlpascua@gmail.com>
2017-11-19 14:48:46 -0800

    tested contact events

---
+++ game/main_core/platform.js
@@ -0,0 +1,30 @@
+function buildPlatforms(){
+  console.log("BUILDING TEMPLATE BLCOKS");
+  sprites = game.add.group();
+
+	for (var i = 0; i < 10; i++){
+		var s = sprites.create(game.rnd.integerInRange(100, 700), game.rnd.integerInRange(32, 200), 'level1');
+		game.physics.enable(s, Phaser.Physics.ARCADE);
+		s.body.velocity.x = game.rnd.integerInRange(-200, 200);
+		s.body.velocity.y = game.rnd.integerInRange(-200, 200);
+	}
+
+	var groupB = game.make.group();
+	groupB.create(16, 16, 'phaser');
+	sprites.add(groupB);
+
+	sprites.setAll('body.collideWorldBounds', true);
+	sprites.setAll('body.bounce.x', 1);
+	sprites.setAll('body.bounce.y', 1);
+	sprites.setAll('body.minBounceVelocity', 0);
+}
+
+function processCallback (obj1, obj2) {
+	console.log("Process Call buck");
+}
+
+function collisionCallback (obj1, obj2) {
+	console.log("Collision Callbuck");
+
+}
+
