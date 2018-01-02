1acc6202f71ba91545c9cbb73e036ea4d4d85514
Ted Pascua <tedlpascua@gmail.com>
2018-01-01 19:48:43 -0800

    map test

---
+++ game/assets/Tiled/Tiles/Basic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="basic" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../../tile_set/basic.png" width="320" height="320"/>
+</tileset>
